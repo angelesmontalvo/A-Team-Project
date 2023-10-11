5224c558c13660bff70461d3df63069e9f1271cb
Angeles Montalvo <angeles.montalvo.267@my.csun.edu>
2023-10-11 14:22:14 -0700

    Test changes

--- TestJavaProject/src/testpackage/testclass.java
+++ TestJavaProject/src/testpackage/testclass.java
@@ -3,5 +3,7 @@ package testpackage;
 public class testclass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");}
+		System.out.println("Hello");
+		System.out.println("Hello XIN");
+	}
 }
