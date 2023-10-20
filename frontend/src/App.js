908a8192607b8b5c4cc778f64fc38b436b3f4cf4
Angeles Montalvo <angeles.montalvo.267@my.csun.edu>
2023-10-19 23:41:59 -0700

    Bootstrap setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
-function App() {
+const App = () => {
   return (
     <>
-    <h1>Welcome to our project</h1>
+      <Header />
+      <main>
+        <h1>Welcome to our project</h1>
+      </main>
+      <Footer />
     </>
   );
 }
