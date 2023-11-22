7d73178b3d7e9b81bfa2ef01c870c781f3559f5f
Angeles Montalvo <angeles.montalvo.267@my.csun.edu>
2023-11-21 22:01:13 -0800

    documentation
    had to redo the documentation because something was merged into main branch causing the documentation to be lost

--- frontend/prdctdetails.js
+++ frontend/prdctdetails.js
@@ -1,3 +1,11 @@
+/*
+    PRICE UPDATE
+    November 15, 2023
+    Angeles Montalvo
+    This javascript file is used to update price of product based on size selected
+*/
+
+//updatePrice(): updates price based on the value of size selected
 function updatePrice() {
     var dropdown = document.getElementById('sizeDropdown');
     var selectedSize = dropdown.options[dropdown.selectedIndex].value;
